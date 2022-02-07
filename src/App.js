65a39a26aeb43ab7ee4d98af03ec05db31cc94a9
proloypaul <proloyp23@gmail.com>
2022-02-07 12:32:40 +0600

    created blogs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Blogs from './Share/Blogs/Blogs';
 
 function App() {
   return (
     <div>
       <h1>Iman Express</h1>
+      <Blogs></Blogs>
     </div>
   );
 }
---
+++ src/Share/Blogs/Blogs.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import { Grid, Paper } from '@mui/material';
+import "./Blogs.css"
+
+const Blogs = () => {
+    const blogApi = [
+        {
+            id: 1,
+            img: "https://pathao.com/bn/wp-content/uploads/sites/6/2021/06/AddToCart_Blog-348x224.png",
+            category: "Food",
+            description: "I something missing. Bolg artical",
+            date: "January 1, 2022"
+        },
+        {
+            id: 1,
+            img: "https://pathao.com/bn/wp-content/uploads/sites/6/2021/06/I-_Pathao-Food_Rajdhanir-Shera_Rajdhanir-Shera-Burger_Blog-1-348x224.jpg",
+            category: "Food",
+            description: "I something missing. Bolg artical",
+            date: "January 1, 2022"
+        },
+        {
+            id: 1,
+            img: "https://pathao.com/bn/wp-content/uploads/sites/6/2021/06/Blog_Pathao-Courier_ঢাকার-পার্শ্ববর্তী-এলাকায়-ডেলিভারি-৮০-টাকায়-348x224.jpg",
+            category: "Food",
+            description: "I something missing. Bolg artical",
+            date: "January 1, 2022"
+        }
+    ]
+    return (
+        <div className='blogContainer'>
+            <Container>
+                <div>
+                    <div className='moreBlogs'>
+                        <h1>More Blogs</h1>
+                        <button>Go to Blogs</button>
+                    </div>
+                <Grid container spacing={2}>
+                    {blogApi.map(blog => <Grid item xs={12} md={4} sm={6} key={blog._id}>
+                        <Paper elevation={12}>
+                            <div className='blogBox'>
+                                <div>
+                                    <img src={blog.img} alt="Empty!" style={{width:"100%", height: "200px", borderRadius: "5px"}}/>
+                                </div>
+                                <div className='blogBoxContent'>
+                                    <div className='blogTitle'>
+                                        <span>{blog.category}</span>
+                                        <li>{blog.date}</li>
+                                    </div>
+                                    <p>{blog.description}</p>
+                                </div>
+                            </div>
+                        </Paper>
+                    </Grid>)}
+                    </Grid>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default Blogs;
