8ebb2eb0bfd49b22fab589ce0a9d274c0d504ccf
proloypaul <proloyp23@gmail.com>
2022-02-14 20:17:44 +0600

    Product update section on blog section

--- src/Component/Blogs/BlogsHome/BlogsHome.js
+++ src/Component/Blogs/BlogsHome/BlogsHome.js
@@ -1,6 +1,6 @@
 import { Container, Grid, Paper } from '@mui/material';
 import React from 'react';
-import Blogs from '../Blogs';
+import ProductUpdate from '../ProductUpdate/ProductUpdate';
 import './BlogsHome.css';
 
 const BlogsHome = () => {
@@ -8,21 +8,21 @@ const BlogsHome = () => {
     const blogApi = [
         {
             id: 1,
-            img: "https://i.ibb.co/TYGPCH2/Food-delivery-boy-driving-scooter-with-box-with-food-and-wearing-mask.jpg",
+            img: "https://pathao.com/wp-content/uploads/2021/10/Insurance-Safety-Pathao_-1536x715-1-348x224.png",
             category: "Iman Rider",
             description: "Iman Xpress Send your cart to food update",
             date: "Jan 1, 2022"
         },
         {
             id: 2,
-            img: "https://i.ibb.co/dt5Hmd5/Baked-chicken-wings-in-the-Asian-style-and-tomatoes-sauce-on-plate.jpg",
+            img: "https://pathao.com/wp-content/uploads/2021/08/Blog-1-348x224.jpg",
             category: "Iman Food",
             description: "Looking for all the burgers in Dhaka",
             date: "Jan 4, 2022"
         },
         {
             id: 3,
-            img: "https://i.ibb.co/wznDZgT/home-Blog-Pic-Three.jpg",
+            img: "https://pathao.com/wp-content/uploads/2021/07/FreeTreat_Blog-4-348x224.png",
             category: "Iman Courier",
             description: "Iman Xpress Send courier now more affordable!",
             date: "Jan 9, 2022"
@@ -52,7 +52,7 @@ const BlogsHome = () => {
                     <Grid container spacing={2}>
                         <Grid item xs={12} md={6} sm={6} >
                             <Paper elevation={4}>
-                                <div className='blogBox'>
+                                <div className='blogBoxThree'>
                                     <div>
                                         <img src='https://pathao.com/wp-content/uploads/2021/06/Blog-3-538x343.jpg' alt="Empty!" style={{width:"100%", height: "400px", borderRadius: "5px"}}/>
                                     </div>
@@ -77,12 +77,13 @@ const BlogsHome = () => {
                                         <p>{blog.description}</p>
                                     </div>
                                     <div>
-                                        <img src='https://pathao.com/wp-content/uploads/2021/06/Blog-3-538x343.jpg' alt="Empty!"/>
+                                        <img src={blog.img} alt="Empty!"/>
                                     </div>
                                 </div>
                             </Paper>)}
                         </Grid>
                     </Grid>
+                    <ProductUpdate></ProductUpdate>
                 </Container>
             </div>
         </div>
---
+++ src/Component/Blogs/ProductUpdate/ProductUpdate.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import Container from '@mui/material/Container';
+import { Grid, Paper } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+const ProductUpdate = () => {
+    const blogApiTwo = [
+        {
+            id: 1,
+            img: "https://pathao.com/wp-content/uploads/2021/10/Insurance-Safety-Pathao_-1536x715-1-348x224.png",
+            category: "Bangladesh",
+            description: "Safe Trips, Insured! ",
+            date: "Feb 01, 2021"
+        },
+        {
+            id: 2,
+            img: "https://pathao.com/wp-content/uploads/2021/08/Blog-1-348x224.jpg",
+            category: "Iman Bike",
+            description: "Now You Can Send Voice Messages! ",
+            date: "Mar 4, 2022"
+        },
+        {
+            id: 3,
+            img: "https://pathao.com/wp-content/uploads/2021/07/FreeTreat_Blog-4-348x224.png",
+            category: "Iman Food",
+            description: "Iman Food Presents Food’s On Me",
+            date: "January 9, 2022"
+        }
+    ]
+    return (
+        <div className='blogContainer'>
+            <Container>
+                <div>
+                    <div className='moreBlogs'>
+                        <h1>Product Update</h1>
+                        <Link to="/moreBlogs">
+                            <button className='allBlogsBtn'>
+                            <p>View all</p>
+                            <i><i class="fas fa-arrow-right"></i></i>
+                            </button>
+                        </Link>
+                    </div>
+                <Grid container spacing={2}>
+                    {blogApiTwo.map(blog => <Grid item xs={12} md={4} sm={6} key={blog.id}>
+                        <Paper elevation={4}>
+                            <div className='blogBox'>
+                                <div>
+                                    <img src={blog.img} alt="Empty!" style={{width:"100%", height: "200px", borderRadius: "5px"}}/>
+                                </div>
+                                <div className='blogBoxContent'>
+                                    <div className='blogTitle'>
+                                        <span>{blog.category}</span>
+                                        <li>{blog.date}</li>
+                                    </div>
+                                    <p>{blog.description}</p>
+                                </div>
+                            </div>
+                        </Paper>
+                    </Grid>)}
+                    </Grid>
+                </div>
+            </Container>
+        </div>
+    );
+};
+
+export default ProductUpdate;
