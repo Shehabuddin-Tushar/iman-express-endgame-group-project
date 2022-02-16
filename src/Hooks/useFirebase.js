a659ebbdd3b41a9c9d40cb8bde70601be7a12fc1
ImtiazMahmod <imtiazmahmod2@gmail.com>
2022-02-16 20:40:09 +0600

    redirect

--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -4,10 +4,14 @@ import {
   GoogleAuthProvider,
   onAuthStateChanged,
   signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  updateProfile,
   signOut,
 } from "firebase/auth";
 
 import initAuth from "../Pages/Login/firebase.init";
+import axios from "axios";
 initAuth();
 const useFirebase = () => {
   const auth = getAuth();
@@ -26,11 +30,12 @@ const useFirebase = () => {
   }, []);
 
   ///google login
-  const googleLogin = () => {
+  const googleLogin = (redirect, navigate) => {
     signInWithPopup(auth, provider)
       .then((result) => {
         const user = result.user;
         // ...
+        navigate(redirect);
       })
       .catch((error) => {
         // Handle Errors here.
@@ -38,7 +43,35 @@ const useFirebase = () => {
         const errorMessage = error.message;
       });
   };
+  ///new User register
+  const registerUser = (email, password, name) => {
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        // ...
+        updateUser(name);
+        const newUser = {
+          displayName: name,
+          email: user.email,
+        };
+        newUserInfo(newUser);
+      })
+      .catch((error) => {
+        // ..
+      });
+  };
 
+  ///login user
+  const userLogin = (email, password, redirect, navigate) => {
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        navigate(redirect);
+      })
+      .catch((error) => {});
+  };
   ///logOUt
   const logOut = () => {
     signOut(auth)
@@ -50,8 +83,32 @@ const useFirebase = () => {
       });
   };
 
+  ///user info post to server
+  const newUserInfo = (newUser) => {
+    axios.post("/", newUser).then((res) => {
+      ///save to database
+    });
+  };
+
+  ///update user profile
+  const updateUser = (name) => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        // Profile updated!
+        // ...
+      })
+      .catch((error) => {
+        // An error occurred
+        // ...
+      });
+  };
+
   return {
     googleLogin,
+    registerUser,
+    userLogin,
     logOut,
   };
 };
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,4 +1,5 @@
 import {
+  Alert,
   Button,
   Container,
   Divider,
@@ -9,16 +10,39 @@ import {
 } from "@mui/material";
 import { grey, red } from "@mui/material/colors";
 import { Box, color } from "@mui/system";
-import React from "react";
+import React, { useState } from "react";
 import { useForm } from "react-hook-form";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import useFirebase from "../../Hooks/useFirebase";
 
 const Login = () => {
-  const { googleLogin } = useFirebase();
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+  const { googleLogin, userLogin } = useFirebase();
   const { register, handleSubmit, reset } = useForm();
+
+  ///redirect user  destination
+  const location = useLocation();
+  const redirect = location?.state?.from || "/";
+  console.log(redirect);
+  const navigate = useNavigate();
+
   const onSubmit = (data) => {
+    const email = data.email;
+    const password = data.password;
+    try {
+      userLogin(email, password, redirect, navigate);
+      setSuccess(true);
+      reset();
+    } catch {
+      setError(true);
+    }
     console.log(data);
+  };
+
+  ///handle google login
+  const handleGoogle = () => {
+    googleLogin(redirect, navigate);
   };
 
   return (
@@ -94,19 +118,30 @@ const Login = () => {
                     </Link>
                   </Typography>
                 </Box>{" "}
-                <Divider sx={{ mt: 5 }} />
-                <Typography variant="h6" sx={{ my: 2 }}>
-                  Alternative Login
-                </Typography>
-                <Button
-                  sx={{ width: "100%" }}
-                  variant="outlined"
-                  color="warning"
-                  onClick={googleLogin}
-                >
-                  Google Login
-                </Button>
               </form>
+              <Divider sx={{ mt: 5 }} />
+              <Typography variant="h6" sx={{ my: 2 }}>
+                Alternative Login
+              </Typography>
+              <Button
+                sx={{ width: "100%" }}
+                variant="outlined"
+                color="warning"
+                onClick={handleGoogle}
+              >
+                Google Login
+              </Button>
+
+              {error && (
+                <Alert sx={{ my: 2 }} severity="error">
+                  Password not matched.
+                </Alert>
+              )}
+              {success && (
+                <Alert sx={{ my: 2 }} severity="success">
+                  Account successfully created.
+                </Alert>
+              )}
             </Paper>
           </Grid>
         </Grid>
