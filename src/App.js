f630d2d52bb9d915fd4da557c44940c73fcc91fb
unknown <AnikBarua34>
2022-02-17 17:10:59 +0600

    about us contact us

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Box } from "@mui/material";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
+import ContactUs from "./Pages/ContactUs/ContactUs";
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
 import Register from "./Pages/Login/Register";
+import AboutUs from './Pages/AboutUs/AboutUs';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
              <Route path="/" element={<Home></Home>}></Route>
              <Route path="/login" element={<Login></Login>}></Route>
              <Route path="/register" element={<Register></Register>}></Route>
+             <Route path="/contactUs" element={<ContactUs></ContactUs>}></Route>
+             <Route path="/aboutUs" element={<AboutUs></AboutUs>}></Route>
          </Routes>
       </Router>
       
